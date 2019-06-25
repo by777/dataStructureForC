c3942ef56eaaa00c67fc3828cc7890b32eba37fe
Xu Bai <1373953675@qq.com>
2019-06-26 00:36:57 +0800

    顺序表（未完成）

---
+++ _01.线性表顺序存储.c
@@ -0,0 +1,93 @@
+/*
+ * @Author: Xu Bai
+ * @Date: 2019-06-25 23:10:17
+ * @LastEditors: Xu Bai
+ * @LastEditTime: 2019-06-26 00:10:55
+ */
+#define MAXSIZE 20
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+typedef int Status;
+typedef int ElemType;
+
+Status visit(ElemType c){
+    printf("%d",c);
+    return OK;
+}
+
+typedef struct 
+{
+    ElemType data[MAXSIZE];
+    int length;
+}SqList;
+
+Status InitList(SqList *L){
+    L ->length = 0;
+    return OK;
+}
+
+Status ListEmpty(SqList L){
+    if (L.length == 0)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }    
+}
+
+Status ClearList(SqList *L){
+    L ->length = 0;
+    return OK;
+}
+
+int ListLength(SqList L){
+    return L.length;
+}
+
+Status GetElem(SqList L, ElemType *e, int i){
+    if (L.length == 0 || i < 1 || i > L.length)
+    {
+        return ERROR;
+    }
+    *e = L.data[i-1]; // 返回第i个元素，下标为i -1
+    return OK;    
+}
+
+int LocateElem(SqList L, ElemType e){
+    if (L.length == 0)
+    {
+        return ERROR;
+    }
+    int i ;
+    for ( i = 0; i < L.length; i++)// 长度为5，下标为0~4
+    {
+        if (L.data[i] == e)
+        {
+            return i + 1;
+        }
+        
+    }
+    return 0;    
+}
+
+Status ListInert(SqList *L, ElemType e, int i){
+    // 在第i个位置之前插入新元素e
+    // 初始条件：L存在且不满,且i<=length
+    if (L ->length == MAXSIZE || i < 1 || i > L ->length)
+    {
+        return ERROR;
+    }
+    int k; 
+    // 插入新元素
+    for (k = L ->length -1; ;)
+    {
+        /* code */
+    }
+    
+    
+}
