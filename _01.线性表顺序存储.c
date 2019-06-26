09d88ab18155f93003c26b375573cf869842d0a1
Xu Bai <1373953675@qq.com>
2019-06-26 15:04:19 +0800

    111

--- _01.线性表顺序存储.c
+++ _01.线性表顺序存储.c
@@ -2,7 +2,7 @@
  * @Author: Xu Bai
  * @Date: 2019-06-25 23:10:17
  * @LastEditors: Xu Bai
- * @LastEditTime: 2019-06-26 14:59:46
+ * @LastEditTime: 2019-06-26 15:04:12
  */
 
 #include "stdio.h"
@@ -134,7 +134,7 @@ int main(){
         
     }
     ListTraverse(L);
-    //ceshi
+    //ceshi11
     getchar();
 
 
