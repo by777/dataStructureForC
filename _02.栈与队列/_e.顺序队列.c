2cb85d4c7fb19b13c6f7cb38c4a4824148c77e0b
Xu Bai <1373953675@qq.com>
2019-07-02 22:33:33 +0800

    顺序队列初步

---
+++ _02.栈与队列/_e.顺序队列.c
@@ -0,0 +1,141 @@
+/*
+ * @Author: Xu Bai
+ * @Date: 2019-07-02 22:06:31
+ * @LastEditors: Xu Bai
+ * @LastEditTime: 2019-07-02 22:32:59
+ */
+#include "stdlib.h"
+#include "io.h"
+#include "stdio.h"
+
+#define MAXSIZE 20
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+typedef int Status;
+typedef int ElemType;
+
+typedef struct
+{
+    ElemType data[MAXSIZE];
+    int front;
+    int rear;
+} SqQueue;
+
+Status visit(ElemType e)
+{
+    printf("%d ", e);
+    return OK;
+}
+
+Status InitQueue(SqQueue *Q)
+{
+    Q->front = 0;
+    Q->rear = 0;
+    return OK;
+}
+
+Status CleatQueue(SqQueue *Q)
+{
+    Q->front = Q->rear = 0;
+    return OK;
+}
+
+Status QuequEmpty(SqQueue Q)
+{
+    if (Q.front == Q.rear)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int QueueLength(SqQueue Q)
+{
+    return (Q.rear - Q.front + MAXSIZE) % MAXSIZE;
+}
+
+Status GetHead(SqQueue Q, ElemType *e)
+{
+    if (Q.front = Q.rear)
+    {
+        return ERROR;
+    }
+    else
+    {
+        *e = Q.data[Q.front];
+        return OK;
+    }
+}
+
+Status EnQueue(SqQueue *Q, ElemType e)
+{
+    if ((Q->rear + 1) % MAXSIZE == Q->front)
+    {
+        // ¶ÓÁÐÂú
+        return ERROR;
+    }
+    else
+    {
+        Q->data[Q->rear] = e;
+        Q->rear = (Q->rear + 1) % MAXSIZE;
+        return OK;
+    }
+}
+
+Status DeQueue(SqQueue *Q, ElemType *e)
+{
+    if (Q->front == Q->rear)
+    {
+        return ERROR;
+    }
+    *e = Q->data[Q->front];
+    Q->front = (Q->front + 1) % MAXSIZE;
+    return OK;
+}
+
+Status QueueTraverse(SqQueue Q)
+{
+    int i = Q.front;
+    while ((i + Q.front) != Q.rear)
+    {
+        visit(Q.data[i]);
+        i = (i + 1) % MAXSIZE;
+    }
+    printf("\n");
+    return OK;
+}
+
+int main(){
+    ElemType e;
+    SqQueue q;
+    InitQueue(&q);
+    int i ;
+    printf("EnQueue!\n");
+    for ( i = 0; i < MAXSIZE; i++)
+    {
+        EnQueue(&q,i);
+    }
+    
+    QueueTraverse(q);
+    printf("DeQueue!\n");
+    for ( i = 10; i < MAXSIZE; i++)
+    {
+        DeQueue(&q,&e);
+    }
+    QueueTraverse(q);
+    CleatQueue(&q);
+    if (QuequEmpty(q))
+    {
+        printf("empty!\n");
+    }
+    getchar();
+    return OK;
+    
+    
+}
